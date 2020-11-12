e3f9a539cf805c08898d9dc1079436fb5473bec0
Prem Shankar Vasudevan <prem.shankar@tredence.com>
2020-11-12 14:51:21 +0530

    img height adjust

--- src/imgitem.js
+++ src/imgitem.js
@@ -4,10 +4,11 @@ export default function ImgItem(props){
     let caption = props.data.CAPTION.split(" ").join("-");
     let id = props.data.ID.split("{").join("");
     id = id.split("}").join("");
+    let imgid = props.data.AR;
     return(
         <div className="image-card">
-            <a href={`https://c8.alamy.com/comp/${id}/${caption}-${id}.jpg`}>
-                <img src={`https://c8.alamy.com/comp/${id}/${caption}-${id}.jpg`} />
+            <a href={`https://c8.alamy.com/comp/${imgid}/${caption}-${imgid}.jpg`}>
+                <img src={`https://c8.alamy.com/comp/${imgid}/${caption}-${imgid}.jpg`} />
             </a>
             <div className="image-captions">
                 <span>{caption}</span>
